8a50341624d701edf5ee32a8a3a955ddfc80c4cd
Eli <jo458405@hotmail.com>
2020-03-26 15:45:17 +0800

    refactoring and add HTML title

--- index.js
+++ index.js
@@ -46,17 +46,24 @@ const view = {
     const rootElement = document.querySelector('#cards')
     rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join("");
   },
-  flipCard(card) {
-    console.log(card)
-    if (card.classList.contains('back')) {
-      // 回傳正面
-      card.classList.remove('back')
-      card.innerHTML = this.getCardContent(Number(card.dataset.index)) 
-      return
-    }
-    // 回傳背面
-    card.classList.add('back')
-    card.innerHTML = null
+  flipCards(...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        // 回傳正面
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+        return
+      }
+      // 回傳背面
+      card.classList.add('back')
+      card.innerHTML = null
+    })
+    
+  },
+  pairCards(...cards) {
+    cards.map(card => {
+      card.classList.add("paired")
+    }) 
   }
 }
 
@@ -73,14 +80,54 @@ const utility = {
 
 
 const model = {
-  revealedCards: []
+  revealedCards: [],
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  }
 }
 
 const controller = {
   currentState: GAME_STATE.FirstCardAwaits,  // 加在第一行
   generateCards() {
     view.displayCards(utility.getRandomNumberArray(52))
-  } 
+  },
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) {
+      return
+    }
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+        view.flipCards(card)
+        model.revealedCards.push(card)
+        // 判斷配對是否成功
+        if (model.isRevealedCardsMatched()) {
+          // 配對成功
+          this.currentState = GAME_STATE.CardsMatched
+          view.pairCards(...model.revealedCards)
+          model.revealedCards = []
+          this.currentState = GAME_STATE.FirstCardAwaits
+
+        } else {
+          // 配對失敗
+          this.currentState = GAME_STATE.CardsMatchFailed
+          setTimeout(this.resetCards, 1000)
+          
+        }
+        break
+    }
+    console.log('this.currentState', this.currentState)
+    console.log('revealedCards', model.revealedCards.map(card => card.dataset.index))
+  },  
+  resetCards() {
+    view.flipCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
 }
 
 
@@ -90,7 +137,7 @@ controller.generateCards()
 
 document.querySelectorAll('.card').forEach(card => {
   card.addEventListener('click', event => {
-    view.flipCard(card)
+    controller.dispatchCardAction(card)
   })
 })
 
