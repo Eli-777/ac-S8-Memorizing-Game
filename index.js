9470fb7ccc064fe61967d06cd8d2bf99a94a31b5
Eli <jo458405@hotmail.com>
2020-03-26 16:22:36 +0800

    add score and triedTimes function

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ const view = {
     cards.map(card => {
       card.classList.add("paired")
     }) 
+  },
+  renderScore(score) {
+    document.querySelector(".score").innerHTML = `Score: ${score}`;
+  },
+
+  renderTriedTimes(times) {
+    document.querySelector(".tried").innerHTML = `You've tried: ${times} times`;
   }
 }
 
@@ -83,7 +90,9 @@ const model = {
   revealedCards: [],
   isRevealedCardsMatched() {
     return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
-  }
+  },
+  score: 0,
+  triedTimes: 0,
 }
 
 const controller = {
@@ -102,11 +111,13 @@ const controller = {
         this.currentState = GAME_STATE.SecondCardAwaits
         break
       case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
         view.flipCards(card)
         model.revealedCards.push(card)
         // 判斷配對是否成功
         if (model.isRevealedCardsMatched()) {
           // 配對成功
+          view.renderScore(model.score += 10)
           this.currentState = GAME_STATE.CardsMatched
           view.pairCards(...model.revealedCards)
           model.revealedCards = []
